fe00b418bb1ef7ea38fff5c8fe1bc3d69e4d805d
LorenaMendes <lorenamendes.contato@gmail.com>
2020-06-19 21:54:08 -0300

    database

--- SandBoxBlog/SandBoxBlog/settings.py
+++ SandBoxBlog/SandBoxBlog/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+PROJECT_DIR = os.path.dirname(__file__)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -119,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = 'SandBoxBlog/main/staticfiles'
+
+STATICFILES_DIRS = (
+    'main/staticfiles',
+)
--- SandBoxBlog/main/models.py
+++ SandBoxBlog/main/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class TimeStamped(models.Model):
+    creation_date = models.DateTimeField()
+    last_modified = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        if not self.creation_date:
+            self.creation_date = timezone.now()
+
+        self.last_modified = timezone.now()
+        return super(TimeStamped, self).save(*args, **kwargs)
+
+    class Meta:
+        abstract = True
+
+class Post(TimeStamped):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    content = models.CharField(max_length=2000)
+
+    def __str__(self):
+        return self.title
